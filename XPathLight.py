2b7e70bc551b230860c3afe5ae59a270e7ee2bdf
Sunil Umasankar <sunil.umasankar1995@gmail.com>
2016-11-22 14:35:55 +0530

    Added fix for StaleElementReferenceException

--- XPathLight.py
+++ XPathLight.py
@@ -4,160 +4,98 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import StaleElementReferenceException
 
 class XPathLight:
     def __init__(self,browser):
         self.browser = browser
-        
-    def find(self,tag="*", attrs={}, idName="", className="",name="",containsText="",xpath="",wait=10):
-        if tag != "*" and len(attrs) == 0:
-            try:
-                element = WebDriverWait(self.browser, wait).until(EC.presence_of_element_located((By.TAG_NAME,tag)))
-            except TimeoutException as e:
-                print "Element Not Found. Timeout " + str(wait) + "s"
-                return False
-            return element
-        if idName != "":
-            try:
-                element = WebDriverWait(self.browser, wait).until(EC.presence_of_element_located((By.ID,idName)))
-            except TimeoutException as e:
-                print "Element Not Found. Timeout " + str(wait) + "s"
-                return False
-            return element
-        if className != "":
-            try:
-                element = WebDriverWait(self.browser, wait).until(EC.presence_of_element_located((By.CLASS_NAME,className)))
-            except TimeoutException as e:
-                print "Element Not Found. Timeout " + str(wait) + "s"
-                return False
-            return element
-        if name != "":
-            try:
-                element = WebDriverWait(self.browser, wait).until(EC.presence_of_element_located((By.NAME,name)))
-            except TimeoutException as e:
-                print "Element Not Found. Timeout " + str(wait) + "s"
-                return False
-            return element
-        if containsText != "":
-            try:
-                element = WebDriverWait(self.browser, wait).until(EC.presence_of_element_located((By.PARTIAL_LINK_TEXT,containsText)))
-            except TimeoutException as e:
-                print "Element Not Found. Timeout " + str(wait) + "s"
-                return False
-            return element
-        if xpath != "":
-            try:
-                element = WebDriverWait(self.browser, wait).until(EC.presence_of_element_located((By.XPATH,xpath)))
-            except TimeoutException as e:
-                print "Element Not Found. Timeout " + str(wait) + "s"
-                return False
-            return element
+
+    def element(self,by=By.ID,value="",wait=10):
         try:
-            element = WebDriverWait(self.browser, wait).until(EC.presence_of_element_located((By.XPATH,"//"+tag+"[contains(@"+attrs.keys()[0]+",'"+attrs.values()[0]+"')]")))
+            element = WebDriverWait(self.browser, wait).until(EC.presence_of_element_located((by,value)))
         except TimeoutException as e:
             print "Element Not Found. Timeout " + str(wait) + "s"
             return False
+        except StaleElementReferenceException as e:
+            try:
+                element = WebDriverWait(self.browser, wait).until(EC.presence_of_element_located((by,value)))
+            except StaleElementReferenceException as e:
+                print "Element not attached after 2 tries"
+                return False
         return element
-    
-    def findAll(self,tag="*", attrs={}, idName="", className="",name="",containsText="",xpath="",wait=10):
-        if tag != "*" and len(attrs) == 0:
-            try:
-                elements = WebDriverWait(self.browser, wait).until(EC.presence_of_all_elements_located((By.TAG_NAME,tag)))
-            except TimeoutException as e:
-                print "Element Not Found. Timeout " + str(wait) + "s"
-                return []
-            return elements
-        if idName != "":
-            try:
-                elements = WebDriverWait(self.browser, wait).until(EC.presence_of_all_elements_located((By.ID,idName)))
-            except TimeoutException as e:
-                print "Element Not Found. Timeout " + str(wait) + "s"
-                return []
-            return elements
-        if className != "":
-            try:
-                elements = WebDriverWait(self.browser, wait).until(EC.presence_of_all_elements_located((By.CLASS_NAME,className)))
-            except TimeoutException as e:
-                print "Element Not Found. Timeout " + str(wait) + "s"
-                return []
-            return elements
-        if name != "":
-            try:
-                elements = WebDriverWait(self.browser, wait).until(EC.presence_of_all_elements_located((By.NAME,name)))
-            except TimeoutException as e:
-                print "Element Not Found. Timeout " + str(wait) + "s"
-                return []
-            return elements
-        if containsText != "":
-            try:
-                elements = WebDriverWait(self.browser, wait).until(EC.presence_of_all_elements_located((By.PARTIAL_LINK_TEXT,containsText)))
-            except TimeoutException as e:
-                print "Element Not Found. Timeout " + str(wait) + "s"
-                return []
-            return elements
-        if xpath != "":
-            try:
-                elements = WebDriverWait(self.browser, wait).until(EC.presence_of_all_elements_located((By.XPATH,xpath)))
-            except TimeoutException as e:
-                print "Element Not Found. Timeout " + str(wait) + "s"
-                return []
-            return elements
+
+    def elements(self,by=By.ID,value="",wait=10):
         try:
-            elements = WebDriverWait(self.browser, wait).until(EC.presence_of_all_elements_located((By.XPATH,"//"+tag+"[contains(@"+attrs.keys()[0]+",'"+attrs.values()[0]+"')]")))
+            elements = WebDriverWait(self.browser, wait).until(EC.presence_of_all_elements_located((by,value)))
         except TimeoutException as e:
             print "Element Not Found. Timeout " + str(wait) + "s"
             return []
+        except StaleElementReferenceException as e:
+            try:
+                elements = WebDriverWait(self.browser, wait).until(EC.presence_of_all_elements_located((by,value)))
+            except StaleElementReferenceException as e:
+                print "Elements not attached after 2 tries"
+                return False
         return elements
+
+    def click_element(self,by=By.ID,value="",wait=10):
+        try:
+            element = WebDriverWait(self.browser, wait).until(EC.element_to_be_clickable((by,value)))
+        except TimeoutException as e:
+            print "Element Not Found. Timeout " + str(wait) + "s"
+            return []
+        except StaleElementReferenceException as e:
+            try:
+                element = WebDriverWait(self.browser, wait).until(EC.element_to_be_clickable((by,value)))
+            except StaleElementReferenceException as e:
+                print "Elements not attached after 2 tries"
+                return False
+        return element
+    
+    def find(self,tag="*", attrs={}, idName="", className="",name="",containsText="",xpath="",wait=10):
+        if tag != "*" and len(attrs) == 0:
+            return self.element(By.TAG_NAME,tag,wait)
+        if idName != "":
+            return self.element(By.ID,idName,wait)
+        if className != "":
+            return self.element(By.CLASS_NAME,className,wait)
+        if name != "":
+            return self.element(By.NAME,name,wait)
+        if containsText != "":
+            return self.element(By.PARTIAL_LINK_TEXT,containsText,wait)
+        if xpath != "":
+            return self.element(By.XPATH,xpath,wait)
+        return self.element(By.XPATH,"//"+tag+"[contains(@"+attrs.keys()[0]+",'"+attrs.values()[0]+"')]",wait)
+    
+    def findAll(self,tag="*", attrs={}, idName="", className="",name="",containsText="",xpath="",wait=10):
+        if tag != "*" and len(attrs) == 0:
+            return self.elements(By.TAG_NAME,tag,wait)
+        if idName != "":
+            return self.elements(By.ID,idName,wait)
+        if className != "":
+            return self.elements(By.CLASS_NAME,className,wait)
+        if name != "":
+            return self.elements(By.NAME,name,wait)
+        if containsText != "":
+            return self.elements(By.PARTIAL_LINK_TEXT,containsText,wait)
+        if xpath != "":
+            return self.elements(By.XPATH,xpath,wait)
+        return self.elements(By.XPATH,"//"+tag+"[contains(@"+attrs.keys()[0]+",'"+attrs.values()[0]+"')]",wait)
     
     def clickable(self,tag="*", attrs={}, idName="", className="",name="",containsText="",xpath="",wait=10):
         if tag != "*" and len(attrs) == 0:
-            try:
-                element = WebDriverWait(self.browser, wait).until(EC.element_to_be_clickable((By.TAG_NAME,tag)))
-            except TimeoutException as e:
-                print "Element Not Found. Timeout " + str(wait) + "s"
-                return False
-            return element
+            return self.click_element(By.TAG_NAME,tag,wait)
         if idName != "":
-            try:
-                element = WebDriverWait(self.browser, wait).until(EC.element_to_be_clickable((By.ID,idName)))
-            except TimeoutException as e:
-                print "Element Not Found. Timeout " + str(wait) + "s"
-                return False
-            return element
+            return self.click_element(By.ID,idName,wait)
         if className != "":
-            try:
-                element = WebDriverWait(self.browser, wait).until(EC.element_to_be_clickable((By.CLASS_NAME,className)))
-            except TimeoutException as e:
-                print "Element Not Found. Timeout " + str(wait) + "s"
-                return False
-            return element
+            return self.click_element(By.CLASS_NAME,className,wait)
         if name != "":
-            try:
-                element = WebDriverWait(self.browser, wait).until(EC.element_to_be_clickable((By.NAME,name)))
-            except TimeoutException as e:
-                print "Element Not Found. Timeout " + str(wait) + "s"
-                return False
-            return element
+            return self.click_element(By.NAME,name,wait)
         if containsText != "":
-            try:
-                element = WebDriverWait(self.browser, wait).until(EC.element_to_be_clickable((By.PARTIAL_LINK_TEXT,containsText)))
-            except TimeoutException as e:
-                print "Element Not Found. Timeout " + str(wait) + "s"
-                return False
-            return element
+            return self.click_element(By.PARTIAL_LINK_TEXT,containsText,wait)
         if xpath != "":
-            try:
-                element = WebDriverWait(self.browser, wait).until(EC.element_to_be_clickable((By.XPATH,xpath)))
-            except TimeoutException as e:
-                print "Element Not Found. Timeout " + str(wait) + "s"
-                return False
-            return element
-        try:
-            element = WebDriverWait(self.browser, wait).until(EC.element_to_be_clickable((By.XPATH,"//"+tag+"[contains(@"+attrs.keys()[0]+",'"+attrs.values()[0]+"')]")))
-        except TimeoutException as e:
-            print "Element Not Found. Timeout " + str(wait) + "s"
-            return False
-        return element      
+            return self.click_element(By.XPATH,xpath,wait)
+        return self.click_element(By.XPATH,"//"+tag+"[contains(@"+attrs.keys()[0]+",'"+attrs.values()[0]+"')]",wait)
             
     def text(self,tag="*", attrs={}, idName="", className="",name="",containsText="",xpath="",wait=10):
         element = self.find(tag,attrs,idName,className,name,containsText,xpath,wait)
@@ -176,10 +114,12 @@ class XPathLight:
             return False
     
     def click(self,tag="*", attrs={}, idName="", className="",name="",containsText="",xpath="",wait=10):
-        element = self.clickable(tag,attrs,idName,className,name,containsText,xpath,wait)
+        element = self.find(tag,attrs,idName,className,name,containsText,xpath,wait)
         if element:
-            element.click()
-            return True
+            if self.clickable(tag,attrs,idName,className,name,containsText,xpath,wait):
+                element.click()
+                return True
+            return False
         else:
             return False
         
